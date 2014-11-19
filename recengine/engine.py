51dffb9d1b81ff0321605c06f2799e4a3ddc3d03
collfi <valentovic.boris@gmail.com>
2014-11-19 20:43:23 +0100

    collaborative filtering

--- recengine/engine.py
+++ recengine/engine.py
@@ -4,8 +4,8 @@ from mongokit import Connection
 import sys
 import math
 # i need to add this because of imports
-sys.path.append('/home/michal/Desktop/RECSYS/RecipeRecommender/')
-#sys.path.append('/home/collfi/RecSys/RecipeRecommender/')
+#sys.path.append('/home/michal/Desktop/RECSYS/RecipeRecommender/')
+sys.path.append('/home/collfi/RecSys/RecipeRecommender/')
 from sqlalchemy import and_
 from webapp.models import recommender
 from datetime import datetime
@@ -106,50 +106,41 @@ def hackernews_score(votes, item_hour_age, gravity=1.8):
 
 #region personalized
 #region collaborative filtering
+
+
 def collaborative_filtering():
-  '''
-  for every user
-    for every item
-      if not in user['ratings']
-        sort similar user by how many common items they have #useless????????????????
+  for u in userscol.User.find():
+    pred = []
+
+    for r in recipecol.Recipe.find():
+
+      if u.getRating(r['_id']) is None:
 
         numerator = 0
         denominator = 0
-        for every similar user(b)
-          if item in b['ratings']
-            numerator += pearson_sim_user(user, b) * (rating(b, item) - average(b))
-            denominator += pearson_si_user(user, b)
 
-        pred(user, item) = average(user) + (numerator / denominator)
+        for b in u['similiar_users']:
+          #rating
+          user_b  = userscol.User.find_one({'_id': b['userid']})
+          if u == user_b: break
+          rating = user_b.getRating(r['_id'])
+          if rating is None: break
 
-        save pred to array and sort by value
-        display top 5
+          numerator += (pearson_sim_user(u, user_b) * (rating - average_rating(user_b)))
+          denominator += pearson_sim_user(u, user_b)
 
+        if denominator == 0: break
 
-        /******************************************
-          pred(user,item) = average(u) + (sum(pearson_sim_user(u,b) * rating(b, i))) / (sum(pearson_sim_user(u,b)))
-
-        OR
-
-        //WRONG!
-        for every similar user(b)
-          sum_pearson += pearson_sim_user(user, b)
-        pred(user, item) = average(user) + (sum_pearson * rating(b, item)) / (sum_pearson)
-
-        OR
-
-        as previous, but that for cycle to the first cycle!*/
-        ********************************************/
-
-  .what if they have only few similar items?
-  .in similarity, pick only similar, not exactly 7 every time - pick those, with similarity > 0.7, maximum 7 (20-30)
-
-  '''
-  pass
+        predicted_rating = average_rating(u) + (numerator / denominator)
+        pred.append({'itemid': r['_id'], 'value': predicted_rating})
+    print(u['_id'])
+    print(pred)
+    #TODO sort pred
+    #TODO add to mongo? create field for predicted items
 
 #Pearson Correlation Coefficient
 def pearson_sim_user(user1, user2):
-  if user1['_id'] == user2['_id']: return 0.0 # should be 1.0 but we don't want to compute this
+  #if user1['_id'] == user2['_id']: return 0.0  # should be 1.0 but we don't want to compute this #osefovat a vymazat
   if len(user1['ratings']) == 0 or len(user2['ratings']) == 0: return 0.0
 
   # list of mutual ratings
@@ -159,30 +150,33 @@ def pearson_sim_user(user1, user2):
       if item1['itemid'] == item2['itemid']:
         mratings.append({'itemid': item1['itemid'], 'value1': item1['value'], 'value2': item2['value']})
         break
-
   # if are no common ratings
   if len(mratings) == 0: return 0.0
 
 
   #average ratings for users
-  average1 = 0
-  for item in user1['ratings']:
-    average1 += (item['value'])
-  average1 /= len(user1['ratings'])
-  average2 = 0
-  for item in user2['ratings']:
-    average2 += (item['value'])
-  average2 /= len(user2['ratings'])
-
+  average1 = average_rating(user1)
+  average2 = average_rating(user2)
   den1 = 0
   den2 = 0
   sum1 = 0
   for item in mratings:
-    sum1 += ((item.get('value1') - average1) * (item.get('value2') - average2))
-    den1 += (item.get('value1') - average1) * (item.get('value1') - average1)
-    den2 += (item.get('value2') - average2) * (item.get('value2') - average2)
-  return (sum1)/((sqrt(den1))*(sqrt(den2))) # * 1/min(common items, treshold) OR just put constant in denominator
+    sum1 += ((item['value1'] - average1) * (item['value2'] - average2))
+    den1 += (item['value1'] - average1) ** 2
+    den2 += (item['value2'] - average2) ** 2
+  den = (sqrt(den1)) * (sqrt(den2))
+  if den == 0.0:
+    return 0.0
+  return sum1/den  #TODO: * 1/min(common items, threshold) OR just put constant in denominator
 
+
+def average_rating(user):
+  average = 0.0
+  if len(user['ratings']) != 0:
+    for item in user['ratings']:
+      average += (item['value'])
+    average /= len(user['ratings'])
+  return average
 #endregion
 
 #region content-based
